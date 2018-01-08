93fe2a1bc52ba2a01937f635ba29de9fde2c19eb
ShemarKnight <shemarknight@hotmail.com>
2018-01-08 13:39:47 -0800

    Paddle Background

--- Breakout Game/Breakout Game/Form1.Designer.cs
+++ Breakout Game/Breakout Game/Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BallPaddle = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BallPaddle)).BeginInit();
             this.SuspendLayout();
+            // 
+            // BallPaddle
+            // 
+            this.BallPaddle.BackgroundImage = global::Breakout_Game.Properties.Resources.paddle;
+            this.BallPaddle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BallPaddle.Location = new System.Drawing.Point(282, 541);
+            this.BallPaddle.Name = "BallPaddle";
+            this.BallPaddle.Size = new System.Drawing.Size(249, 27);
+            this.BallPaddle.TabIndex = 0;
+            this.BallPaddle.TabStop = false;
             // 
             // GameClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(836, 580);
+            this.Controls.Add(this.BallPaddle);
             this.Name = "GameClient";
             this.Text = "Breakout";
+            ((System.ComponentModel.ISupportInitialize)(this.BallPaddle)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox BallPaddle;
     }
 }
 
