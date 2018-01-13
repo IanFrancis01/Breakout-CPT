589115d23d7922df171d1780d665130ddb2b315f
Ian Francis <IanMichaelFrancis@outlook.com>
2018-01-13 15:39:13 -0500

    Added shemar's code for ball class onto my version, so that we can be in sync.

--- Breakout Game/Breakout Game/Ball.cs
+++ Breakout Game/Breakout Game/Ball.cs
@@ -10,6 +10,34 @@ namespace Breakout_Game
     class Ball
     {
         //how much of the code for the ball can we put in a ball class?
+        //float speed -
+        //vector2 position
+        //vector2 velocity
+        //collision
+
+        //private fields // members //
+        // speed of ball
+        float speed;
+        int mDimensions;
+        Vector2 mPosition;
+        Vector2 mVelocity;
+
+        //constructors//
+        public Ball()
+        {
+            this.mDimensions = 25;
+            this.mPosition = ;
+        }
+        public Ball(int Dimensions, Vector2 Position, Vector2 Velocity)
+        {
+            this.mDimensions = Dimensions;
+            this.mPosition = Position;
+            this.mVelocity = Velocity;
+        }
+        //properties//
+        public int Dimensions { get { return this.mDimensions; } set { this.mDimensions = value; } }
+        public Vector2 Position { get { return this.mPosition; } set { this.mPosition = value; } }
+        public Vector2 Velocity { get { return this.mVelocity; } set { this.mVelocity = value; } }
 
     }
 }
