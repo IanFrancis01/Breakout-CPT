8cfc8c4fdbb9e8680a28386a2d1b04e871bc371a
Shemarknight <shemarknight@hotmail.com>
2018-01-13 20:01:42 -0500

    ball class progress

--- Breakout Game/Breakout Game/Ball.cs
+++ Breakout Game/Breakout Game/Ball.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Breakout_Game
 {
-    class Ball
+    public partial class Ball : Form
     {
+
         //how much of the code for the ball can we put in a ball class?
         //float speed -
         //vector2 position
@@ -17,27 +20,70 @@ namespace Breakout_Game
 
         //private fields // members //
         // speed of ball
-        float speed;
-        int mDimensions;
+        float Speed = 6;
+        int mDimensionsX;
+        int mDimensionsY;
+        float mAngle;
         Vector2 mPosition;
         Vector2 mVelocity;
+        PictureBox picBall;
+
 
         //constructors//
-        public Ball()
+
+        public void MoveBall()
         {
-            this.mDimensions = 25;
-          //  this.mPosition = ;
+            Position += mVelocity;
+            picBall.Left = (int)Position.X;
+            picBall.Top = (int)Position.Y;
         }
-        public Ball(int Dimensions, Vector2 Position, Vector2 Velocity)
+
+        public Ball(PictureBox picBall, int DimensionsX, int DimensionsY, Vector2 Position, Vector2 Velocity, float Angle)
         {
-            this.mDimensions = Dimensions;
+            this.mDimensionsX = DimensionsX;
+            this.mDimensionsY = DimensionsY;
             this.mPosition = Position;
             this.mVelocity = Velocity;
+            this.mAngle = Angle;
+
+
+            Position = new Vector2((ClientSize.Width - picBall.Width) / 2, (ClientSize.Height - (picBall.Height * 4)));
+            Angle = FindRandomAngle();
+            Velocity = new Vector2((float)(Speed * Math.Cos(Angle)), (float)(Speed * Math.Sin(Angle)));
+
         }
+
+        // methods//
+        public static float FindRandomAngle()
+        {
+            Random RandomAngle = new Random();
+
+            int r = 0;
+            while (r % 2 == 0)
+            {
+                r = RandomAngle.Next(1, 8);
+            }
+            float Angle = MathHelper.ToRadians(r * 45);
+
+            return Angle;
+
+        }
+
+        void Reset()
+        {
+            Speed = 6;
+            Position = new Vector2((ClientSize.Width - picBall.Width) / 2, (ClientSize.Height - (picBall.Height * 4)));
+            float Angle = FindRandomAngle();
+            Velocity = new Vector2((float)(Speed * Math.Cos(Angle)), (float)(Speed * Math.Sin(Angle)));
+        }
+
+
         //properties//
-        public int Dimensions { get { return this.mDimensions; } set { this.mDimensions = value; } }
+        public int DimensionsX { get { return this.mDimensionsX; } set { this.mDimensionsX = value; } }
+        public int DimensionsY { get { return this.mDimensionsY; } set { this.mDimensionsY = value; } }
+        public float Angle { get { return this.mAngle; } set { this.mAngle = value; } }
         public Vector2 Position { get { return this.mPosition; } set { this.mPosition = value; } }
-        public Vector2 Velocity { get { return this.mVelocity; } set { this.mVelocity = value; } }
+
 
     }
 }
